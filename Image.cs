aca6c5690206c092afc8f03582b924c5f6c9445e
David L. Inman <davidlevere@gmail.com>
2018-08-18 18:49:13 -0400

    image

---
+++ Image.cs
@@ -0,0 +1,17 @@
+
+namespace ray1
+{
+    public class Image
+    {
+        public int Width;
+        public int Height;
+        public Pixel[,] Pixels;
+    }
+
+    public class Pixel
+    {
+        public byte Red;
+        public byte Green;
+        public byte Blue;
+    }
+}
