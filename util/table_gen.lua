8d73483c7096a7d5e38876cb93a5bb34a6e48839
David L. Inman <davidlevere@gmail.com>
2018-08-20 23:00:01 -0400

    removed comment

--- util/table_gen.lua
+++ util/table_gen.lua
@@ -31,20 +31,3 @@ function display( t )
 end
 
 print( display( crc_table() ) )
---[[rc_table(void)
-{
-    unsigned long c;
-    int n, k;
-                                  
-    for (n = 0; n < 256; n++) {
-        c = (unsigned long) n;
-        for (k = 0; k < 8; k++) {
-            if (c & 1)  c = 0xedb88320L ^ (c >> 1);
-            else
-                c = c >> 1;   
-            }
-           crc_table[n] = c;
-        }
-    crc_table_computed = 1;
-}
---]]
