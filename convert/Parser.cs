b8439450134075fea6093131489221ac6d531de0
David L. Inman <davidlevere@gmail.com>
2018-08-22 21:52:16 -0400

    finished image parser

--- convert/Parser.cs
+++ convert/Parser.cs
@@ -22,28 +22,99 @@ namespace ray1.convert
 
         public Image ParseImageFile()
         {
+            var pixels = new List<(int, int, Pixel)>();
+            int? height = null;
+            int? width = null;
             EatSpace();
             // W( integer )
             // H( integer )
-            // P( h w hex-rgb )
+            // P( h w r g b )
             while ( !End() )
             {
                 var c = _contents[_index];
                 switch (  c )
                 {
                     case 'P':
-                        var  (height, width, pixel) = ParsePixel();
-                        Console.WriteLine( $"{height} {width} {pixel.Red} {pixel.Green} {pixel.Blue}" );
+                        var  pixel = ParsePixel();
+                        pixels.Add( pixel );
                         break;
                     case 'H':
+                        var h = ParseHeight();
+                        if ( height.HasValue )
+                        {
+                            throw new Exception( "double definition of Height" );
+                        }
+                        height = h;
+                        break;
                     case 'W':
+                        var w = ParseWidth();
+                        if ( width.HasValue )
+                        {
+                            throw new Exception( "double definition of Height" );
+                        }
+                        width = w;
+                        break;
                     default:
                     // TODO track line number?
                         throw new Exception( "Error Parsing" );
 
                 }
             }
-            return null;
+            var l = new List<PixelRow>();
+            var pixelArray = pixels.ToArray();
+            for( var h = 1; h <= height.Value; h++ )
+            {
+                var (row, errorColumn) = CreatePixelRow( pixelArray.Where( p => p.Item1 == h ).ToArray(), width.Value );
+                if ( errorColumn.HasValue )
+                {
+                    throw new Exception( $"Row {h} has incorrect number of columns {errorColumn.Value}" );
+                }
+                l.Add( row );
+            }
+            return new Image { Height = height.Value, Width = width.Value, Rows = l };
+        }
+
+        private static (PixelRow, int?) CreatePixelRow( (int, int, Pixel)[] pixelsInRow, int width )
+        {
+            var l = new List<Pixel>();
+            for( var w = 1; w <= width; w++ )
+            {
+                var pixelsInColumn = Array.FindAll( pixelsInRow, p => p.Item2 == w );    
+                if ( pixelsInColumn.Length != 1 )
+                {
+                    return (null, w);
+                }
+                l.Add( pixelsInColumn[0].Item3 );
+            }
+            return (new PixelRow { Row = l }, null);
+        }
+
+        private int ParseHeight()
+        {
+            Require( 'H' );
+            EatSpace();
+            Require( '(' );
+            EatSpace();
+            var height = ParseInt();
+            EatSpace();
+            Require( ')' );
+            EatSpace();
+
+            return height;
+        }
+
+        private int ParseWidth()
+        {
+            Require( 'W' );
+            EatSpace();
+            Require( '(' );
+            EatSpace();
+            var width = ParseInt();
+            EatSpace();
+            Require( ')' );
+            EatSpace();
+
+            return width;
         }
 
         private (int, int, Pixel) ParsePixel()
--- convert/Program.cs
+++ convert/Program.cs
@@ -1,4 +1,4 @@
-
+using System;
 
 namespace ray1.convert
 {
@@ -8,14 +8,40 @@ namespace ray1.convert
         {
             using( var parser = new Parser( 
 @" 
-P( 1234 5678 CD 34 01 )
-P( 1234 5678 CD 34 01 )
 
-P( 1234 5678 CD 34 01 )
+W( 3 )
+
+H( 4 )
+
+P( 1 1 1 D 19 )
+P( 1 2 2 E 1A )
+P( 1 3 3 F 1B)
+P( 2 1 4 10 1C )
+P( 2 2 5 11 1D)
+P( 2 3 6 12 1E)
+P( 3 1 7 13 1F)
+P( 3 2 8 14 20 )
+P( 3 3 9 15 21)
+P( 4 1 A 16 22 )
+P( 4 2 B 17 23 )
+P( 4 3 C 18 24)
 
 ") )
             {
                 var x = parser.ParseImageFile();
+
+                Console.WriteLine( $" h = {x.Height}");
+                Console.WriteLine( $" w = {x.Width}");
+                foreach( var row in x.Rows )
+                {
+                    foreach( var column in row.Row )
+                    {
+                        Console.WriteLine( $" r = {column.Red}");
+                        Console.WriteLine( $" g = {column.Green}");
+                        Console.WriteLine( $" b = {column.Blue}");
+                    }
+                }
+
             }
 
         }
