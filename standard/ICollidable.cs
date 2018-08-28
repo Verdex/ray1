219204fa6a2a586181881e727937a20c6e3893fc
David L. Inman <davidlevere@gmail.com>
2018-08-27 23:31:08 -0400

    inc prog

--- standard/ICollidable.cs
+++ standard/ICollidable.cs
@@ -1,8 +1,14 @@
 
 namespace ray1.standard
 {
+    // todo ambient - color in shadow
+    // todo diffuse - color under white light
+    // todo emissive - color object gives off
+    // todo specular - color object gives off on shiny surface (I think there's an angle at which this occurs at)
+
     public interface Collidable
     {
-        bool Collides( 
+        bool Collides( Point lineStartPoint, Direction lineDirection );
+        
     }
 }
