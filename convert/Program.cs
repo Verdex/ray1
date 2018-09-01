18aa5a326c923daad46f910db4f22b07e3223dfd
David L. Inman <davidlevere@gmail.com>
2018-09-01 15:27:28 -0400

    cleaned up the usage of convert

--- convert/Program.cs
+++ convert/Program.cs
@@ -8,47 +8,18 @@ namespace ray1.convert
     {
         public static void Main(string[] args)
         {
-            /*if ( args.Length != 4 )
-            {
-                Console.WriteLine( "Wrong Number Of Parameters" );
-                Console.WriteLine( "Usage:  Convert.exe -s input.image -d output.png" );
-                return;
-            }
-
-            string inputFileName = null;
-            string outputFileName = null;
-            if ( args[0] == "-s" )
-            {
-                inputFileName = args[1];
-            }
-            else
-            {
-                Console.WriteLine( $"Unknown argument:  {args[0]}" ) ;
-            }
-
-            if ( args[2] == "-d" )
-            {
-                outputFileName = args[3];
-            }
-            else
-            {
-                Console.WriteLine( $"Unknown argument:  {args[2]}" ) ;
-            }*/
+            var name = args[0];
 
 
-            string inputFileName = "Jabber.image";
-            string outputFileName = "Jabber.png";
+            string inputFileName = $"{name}.image";
+            string outputFileName = $"{name}.png";
 
             var inputFile = File.ReadAllText( inputFileName );
 
             using( var parser = new Parser( inputFile ) )
             {
                 var image = parser.ParseImageFile();
-
                 Png.SavePngFromImageUsingBitmap( image, outputFileName );
-                //var bytes = Png.CreatePngFromImage( image );
-                //var bytes = Ppm.CreatePpmFromImage( image );
-                //File.WriteAllBytes( outputFileName, bytes.ToArray() );
             }
         }
     }
